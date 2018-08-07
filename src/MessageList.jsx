b2976575d77e3e4095867c69defd07057ef342c3
Matt Kelly <bmatthewkelly@gmail.com>
2018-08-07 18:45:31 +0000

    update jsx import/export file structure

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,36 +1,78 @@
-const MessageList = [{
-  type: "incomingMessage",
-  content: "I won't be impressed with technology until I can download food.",
-  username: "Anonymous1"
-},
-{
-  type: "incomingNotification",
-  content: "Anonymous1 changed their name to nomnom",
-},
-{
-  type: "incomingMessage",
-  content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
-  username: "Anonymous2"
-},
-{
-  type: "incomingMessage",
-  content: "...",
-  username: "nomnom"
-},
-{
-  type: "incomingMessage",
-  content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
-  username: "Anonymous2"
-},
-{
-  type: "incomingMessage",
-  content: "This isn't funny. You're not funny",
-  username: "nomnom"
-},
-{
-  type: "incomingNotification",
-  content: "Anonymous2 changed their name to NotFunny",
-},
-];
+import React, { Component } from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">
+        <Message />
+      </main>
+    );
+  }
+}
 
 export default MessageList;
+
+// class Message extends Component {
+//   render() {
+//     const generatedMessages = MessageList.map((message) => {
+//       if (message.type === "incomingNotification") {
+//         <div className="message system">
+//           {message.content}
+//         </div>
+//       } else if (message.type === "incomingMessage") {
+//         <div className="message">
+//           <span className="message-username">{message.username}</span>
+//           <span className="message-content">{message.content}</span>
+//         </div>
+//       }
+//     });
+//     return (
+//       <main className="messages">
+//         {generatedMessages}
+//         <div className="message">
+//           <span className="message-username">Test</span>
+//           <span className="message-content">Test</span>
+//         </div>
+//       </main>
+//     )
+//   }
+// }
+
+
+// const MessageList = [{
+//   type: "incomingMessage",
+//   content: "I won't be impressed with technology until I can download food.",
+//   username: "Anonymous1"
+// },
+// {
+//   type: "incomingNotification",
+//   content: "Anonymous1 changed their name to nomnom",
+// },
+// {
+//   type: "incomingMessage",
+//   content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+//   username: "Anonymous2"
+// },
+// {
+//   type: "incomingMessage",
+//   content: "...",
+//   username: "nomnom"
+// },
+// {
+//   type: "incomingMessage",
+//   content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+//   username: "Anonymous2"
+// },
+// {
+//   type: "incomingMessage",
+//   content: "This isn't funny. You're not funny",
+//   username: "nomnom"
+// },
+// {
+//   type: "incomingNotification",
+//   content: "Anonymous2 changed their name to NotFunny",
+// },
+// ];
+
+// export default MessageList;
--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,6 @@ require("../styles/application.scss");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App.jsx';
-import ChatBar from './ChatBar.jsx';
-import Message from './Message.jsx';
+import App from './App.jsx';
 
-ReactDOM.render(<div>
-  <Message />
-  <ChatBar />
-</div>, document.getElementById('react-root'));
+ReactDOM.render(<App />, document.getElementById('react-root'));
